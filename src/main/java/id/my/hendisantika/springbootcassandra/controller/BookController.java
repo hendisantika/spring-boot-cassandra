36e502c880028df103d07aa9cfa41425a9131029
hendisantika <hendisantika@yahoo.co.id>
2024-06-12 10:14:57 +0700

    Add createBook in BookController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootcassandra/controller/BookController.java
+++ src/main/java/id/my/hendisantika/springbootcassandra/controller/BookController.java
@@ -1,15 +1,20 @@
 package id.my.hendisantika.springbootcassandra.controller;
 
+import id.my.hendisantika.springbootcassandra.dto.BookRequest;
 import id.my.hendisantika.springbootcassandra.model.Book;
 import id.my.hendisantika.springbootcassandra.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,5 +37,20 @@ public class BookController {
     public ResponseEntity<List<Book>> getBooks() {
         List<Book> bookList = bookRepository.findAll();
         return ResponseEntity.status(200).body(bookList);
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody BookRequest bookRequest) {
+
+        Book book = new Book();
+        book.setId(UUID.randomUUID());
+        book.setTitle(bookRequest.getTitle());
+        book.setDescription(bookRequest.getDescription());
+        book.setAuthor(bookRequest.getAuthor());
+        book.setYear(bookRequest.getYear());
+        book.setDateCreated(LocalDateTime.now());
+
+        book = bookRepository.save(book);
+        return ResponseEntity.status(201).body(book);
     }
 }
