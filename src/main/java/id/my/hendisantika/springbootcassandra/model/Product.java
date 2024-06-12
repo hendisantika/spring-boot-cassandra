6349cb27c8a8ca450b3a0809129ddfdb7556ca53
hendisantika <hendisantika@yahoo.co.id>
2024-06-12 11:14:57 +0700

    Add Product Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootcassandra/model/Product.java
@@ -0,0 +1,59 @@
+package id.my.hendisantika.springbootcassandra.model;
+
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-cassandra
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/12/24
+ * Time: 11:14
+ * To change this template use File | Settings | File Templates.
+ */
+@Table
+public class Product {
+
+    @PrimaryKeyColumn(name = "product_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+    private final UUID productId;
+
+    @PrimaryKeyColumn(name = "product_name", ordinal = 1, type = PrimaryKeyType.CLUSTERED)
+    private final String productName;
+
+    @Column("description")
+    private final String description;
+
+    @Column("price")
+    private final double price;
+
+    public Product(UUID productId, String productName, String description, double price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productId.equals(product.productId) && productName.equals(product.productName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, productName);
+    }
+}
